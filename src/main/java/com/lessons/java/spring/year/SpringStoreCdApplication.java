78faf8e07b7ea586cda9add111a3408016c93c26
LucaNovelliBoolean <160405407+lucanovellihtml@users.noreply.github.com>
2024-12-28 10:50:10 +0100

    Initial commit

---
+++ src/main/java/com/lessons/java/spring/year/SpringStoreCdApplication.java
@@ -0,0 +1,13 @@
+package com.lessons.java.spring.year;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStoreCdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringStoreCdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lessons/java/spring/year/SpringStoreCdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lessons.java.spring.year;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStoreCdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
